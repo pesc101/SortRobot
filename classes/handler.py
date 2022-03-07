ae591dc9ddf2592aa4d61a746e807cdf1c817a68
Jan Strich <53601317+pesc101@users.noreply.github.com>
2022-03-08 00:45:33 +0100

    Created FileHandler

--- classes/handler.py
+++ classes/handler.py
@@ -1,4 +1,6 @@
 import datetime
+import shutil
+import os
 from watchdog.events import PatternMatchingEventHandler
 
 
@@ -8,10 +10,12 @@ def get_datetime():
 
 class EventHandler(PatternMatchingEventHandler):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, folders, *args, **kwargs):
+        self.folders = folders
         super(EventHandler, self).__init__(*args, **kwargs)
 
     def on_created(self, event):
+        FileHandler(event.src_path).sort_file(folders=self.folders)
         print(f'{get_datetime()} CREATED: {event.src_path} created.')
 
     def on_deleted(self, event):
@@ -22,3 +26,53 @@ class EventHandler(PatternMatchingEventHandler):
 
     def on_moved(self, event):
         print(f'{get_datetime()} MOVED: {event.src_path} >> {event.dest_path}.')
+
+
+class FileHandler:
+
+    def __init__(self, path):
+        self.path = path
+        self.folder_path = self.get_folder_path()
+        self.rest_folder = 'rest'
+        self.typ = self.get_file_typ()
+        self.name = self.get_name()
+        self.num = 0
+
+    def get_file_typ(self):
+        return self.path.split('.')[len(self.path.split('.')) - 1]
+
+    def get_folder_path(self):
+        return "/".join(str(x) for x in self.path.split('/')[0:len(self.path.split('/')) - 1])
+
+    def get_name(self):
+        return self.path.split('/')[len(self.path.split('/')) - 1]
+
+    def get_next_file(self, folder):
+        dest = os.path.join(self.folder_path, folder)
+        while os.path.exists(os.path.join(dest, self.name)):
+            self.num += 1
+
+            period = self.name.rfind('.')
+            if period == -1:
+                period = len(self.name)
+
+            new_file = f'{self.name[:period]}_{self.num}{self.name[period:]}'
+            dest = os.path.join(dest, new_file)
+        return dest
+
+    def sort_file(self, folders):
+        for folder in folders.keys():
+            if self.typ in folders.get(folder):
+                desc_path = os.path.join(self.folder_path, folder, self.name)
+                if os.path.exists(desc_path):
+                    desc_path = self.get_next_file(folder)
+                shutil.move(self.path, desc_path)
+                print(f'{self.path} >>> {desc_path}')
+                return
+
+        # Rest Folder
+        desc_path = os.path.join(self.folder_path, self.rest_folder, self.name)
+        if os.path.exists(desc_path):
+            desc_path = self.get_next_file(self.rest_folder)
+        shutil.move(self.path, desc_path)
+        print(f'{self.path} >>> {desc_path}')
